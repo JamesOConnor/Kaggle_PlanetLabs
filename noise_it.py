45470ec526c57992758ea1f7b5b7693063b8a9b0
JamesOConnor <oconnoj4@tcd.ie>
2016-08-29 21:35:26 +0100

    Add files via upload

---
+++ noise_it.py
@@ -0,0 +1,16 @@
+import skimage.util as noi
+import cv2
+import numpy as np
+import sys
+
+infile=sys.argv[1]
+snr=float(sys.argv[2])
+
+x=cv2.imread(infile).astype(float)
+
+x = x - np.min(x);
+x = x / np.max(x);
+
+v = np.var(x) / (10**(snr/10));
+x_noise=(noi.random_noise(x, mode='gaussian', mean=0, var=v)*255).astype(np.uint8)
+cv2.imwrite('%s'%(infile.replace('.JPG', '_noisy.JPG')), x_noise)
