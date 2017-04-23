631b73d5a3262279174feccf9c4e1a64cf12e933
JamesOConnor <oconnoj4@tcd.ie>
2017-04-23 17:54:02 +0100

    Add files via upload

---
+++ Trained_CNN/CNN_architecture.py
@@ -0,0 +1,38 @@
+def sample_arch(im_size):
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(im_size, im_size, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    #     model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    #     model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu'))
+    #     model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu'))
+    #     model.add(Convolution2D(256, 3, 3, border_mode='same', activation='relu'))
+    #     model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(17, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  # We NEED binary here, since categorical_crossentropy l1 norms the output before calculating loss.
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
